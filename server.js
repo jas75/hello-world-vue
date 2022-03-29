6e07deb1582b93f60907e56b3a27762ffe6d2ee8
jas <jason.azoulay@3ie.fr>
2022-03-29 10:22:33 +0200

    add mock and server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const jsonServer = require("json-server");
+const jwt = require("jsonwebtoken");
+
+
+
+const server = jsonServer.create();
+const router = jsonServer.router('./mock/db.json')
+const userdb = JSON.parse(fs.readFileSync('./mock/users.json', 'UTF-8'))
+
+server.use(jsonServer.defaults());
+server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.json())
+
+const SECRET_KEY = '123456789'
+const expiresIn = '1h'
+
+// Create a token from a payload 
+function createToken(payload) {
+  return jwt.sign(payload, SECRET_KEY, { expiresIn });
+}
+  
+  // Verify the token 
+function verifyToken(token){
+  return jwt.verify(token, SECRET_KEY, (err, decode) =>
+    decode !== undefined ? decode : err
+  );
+}
+  
+  // Check if the user exists in database
+function isAuthenticated({email, password}){
+    return userdb.users.findIndex(user => user.email === email && user.password === password) !== -1
+}
